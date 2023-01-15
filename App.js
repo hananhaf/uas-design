868998df284310c9f8314a20ff468ded6ad52aef
hananhaf <hananhafizh007@gmail.com>
2023-01-16 00:56:30 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,94 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+false;
+import CardBoard1 from "./screens/CardBoard1";
+import HomeDashboard from "./screens/HomeDashboard";
+import Splash from "./screens/Splash";
+import CardBoard3 from "./screens/CardBoard3";
+import CardBoard2 from "./screens/CardBoard2";
+import OrderHistory from "./screens/OrderHistory";
+import Wishlist from "./screens/Wishlist";
+import Browse from "./screens/Browse";
+import SignUp from "./screens/SignUp";
+import Login from "./screens/Login";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setHideSplashScreen(true);
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator
+            initialRouteName="CardBoard1"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen
+              name="CardBoard1"
+              component={CardBoard1}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="HomeDashboard"
+              component={HomeDashboard}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Splash"
+              component={Splash}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="CardBoard3"
+              component={CardBoard3}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="CardBoard2"
+              component={CardBoard2}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="OrderHistory"
+              component={OrderHistory}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Wishlist"
+              component={Wishlist}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Browse"
+              component={Browse}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Splash />
+        )}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;
